80158fca5ad12bd422d85f7070ce6408548e5245
abdelaziz-web <abdel-aziz.achendid@edu.uiz.ac.ma>
2024-09-26 21:49:00 +0100

    Initial commit

---
+++ src/main/java/org/cours/tp1/controller/HelloSpringBoot.java
@@ -0,0 +1,17 @@
+package org.cours.tp1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBoot
+{
+
+    @RequestMapping("/hellospring")
+    public String sayHello() {
+        return "Hello Spring Boot";
+    }
+
+
+}
